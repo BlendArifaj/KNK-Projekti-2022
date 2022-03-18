b14b36f8079f5ed85a98b81e501553dc805b849e
Blend Arifaj <blendarifaj@hotmail.com>
2022-03-18 16:29:16 +0100

    init commit

---
+++ KNK-Projekti/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Button btn = new Button("OK");
+			Pane root = new Pane();
+			root.getChildren().add(btn);
+			Scene scene = new Scene(root,400,400);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
