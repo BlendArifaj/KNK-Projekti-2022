3f8dd4182c3ec1c1576fbd9845aa0a1b51290d82
Blend Arifaj <blendarifaj@hotmail.com>
2022-03-18 16:32:06 +0100

    init commit

---
+++ KNK-Projekti/src/database/database.sql
@@ -0,0 +1,14 @@
+
+DROP TABLE IF EXISTS student;
+CREATE TABLE IF NOT EXISTS student(
+	id int primary key AUTO_INCREMENT,
+	emri varchar(50),
+	mbiemri varchar(50),
+	studentId varchar(14),
+	mosha int
+);
+
+
+select * from student;
+
+# to be continue ...
