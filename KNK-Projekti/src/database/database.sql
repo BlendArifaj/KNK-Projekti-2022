b9fd0154da9f9b8d784cb69faf8e6db7452940ee
Blend Arifaj <blendarifaj@hotmail.com>
2022-03-25 10:08:03 +0100

    database changes

--- KNK-Projekti/src/database/database.sql
+++ KNK-Projekti/src/database/database.sql
@@ -1,5 +1,9 @@
-
 DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS lenda;
+DROP TABLE IF EXISTS profesor;
+DROP TABLE IF EXISTS profesor_lenda;
+DROP TABLE IF EXISTS nota;
+
 CREATE TABLE IF NOT EXISTS student(
 	id int primary key AUTO_INCREMENT,
 	emri varchar(50),
@@ -8,7 +12,33 @@ CREATE TABLE IF NOT EXISTS student(
 	mosha int
 );
 
+CREATE TABLE IF NOT EXISTS lenda (
+	id int primary key AUTO_INCREMENT,
+	emri varchar(50),
+	credits int
+);
 
-select * from student;
+CREATE TABLE IF NOT EXISTS profesor (
+	id int primary key AUTO_INCREMENT,
+	emri varchar(50),
+	titulli varchar(50)
+);
 
-# to be continue ...
+CREATE TABLE IF NOT EXISTS profesor_lenda(
+	id int primary key AUTO_INCREMENT,
+	profesor_id int,
+	lenda_id int,
+	FOREIGN KEY (lenda_id) REFERENCES lenda (id),
+	FOREIGN KEY (profesor_id) REFERENCES profesor (id)
+);
+
+CREATE TABLE IF NOT EXISTS nota(
+	id int primary key AUTO_INCREMENT,
+	student_id int,
+	nota int,
+	lenda_id int,
+	profesor_id int,
+	FOREIGN KEY (student_id) REFERENCES student (id),
+	FOREIGN KEY (lenda_id) REFERENCES lenda (id),
+	FOREIGN KEY (profesor_id) REFERENCES profesor (id)
+);
